1f0114ce1e7e528010320b2bc5870649323ab8bf
Hanno Kügler <hannokuegler@gmailcom>
2019-10-05 20:57:12 +0200

    new pans

---
+++ Shipfights/src/Ships/Hanno.java
@@ -0,0 +1,14 @@
+package Ships;
+
+import javax.swing.JPanel;
+
+public class Hanno extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Hanno() {
+
+	}
+
+}
---
+++ Shipfights/src/Ships/Paul.java
@@ -0,0 +1,14 @@
+package Ships;
+
+import javax.swing.JPanel;
+
+public class Paul extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Paul() {
+
+	}
+
+}
