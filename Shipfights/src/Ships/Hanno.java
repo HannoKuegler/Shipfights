eb55aa167142223c174b78f5030d6a33d90ac641
Hanno Kügler <hannokuegler@gmailcom>
2019-10-05 21:10:15 +0200

    hallo paule

--- Shipfights/src/Ships/Hanno.java
+++ Shipfights/src/Ships/Hanno.java
@@ -1,14 +1,71 @@
 package Ships;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JPanel;
 
-public class Hanno extends JPanel {
+public class Hanno extends JPanel implements MouseListener {
 
-	/**
-	 * Create the panel.
-	 */
+	Graphics2D g; 
+
+	static int width = 1200;
+	static int height = 1200;
+	static int y0 = 200;
+	
+	
 	public Hanno() {
 
 	}
+
+	@Override
+	protected void paintComponent(Graphics gr) {
+		// TODO Auto-generated method stub
+		super.paintComponent(gr);
+	
+		g = (Graphics2D) gr;
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		int mouseX = e.getX(); 
+		int mouseY = e.getY(); 
+
+ 
+		
+		
+		
+		
+	}
+
+	
+	
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 
 }
